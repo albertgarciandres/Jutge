6ca158afc64d522fdfca720fbc6960a944a1d3a9
Albert Garcia <113926575+albertgarciandres@users.noreply.github.com>
2023-06-04 20:00:14 +0200

    Create Multiplication_table.py

---
+++ List 4/Multiplication table/Multiplication_table.py
@@ -0,0 +1,25 @@
+from jutge import read
+
+m = read(int)
+
+one = m*1
+two = m*2
+three = m*3
+four = m*4
+five = m*5
+six = m*6
+seven = m*7
+eight = m*8
+nine = m*9
+ten = m*10
+
+print(str(m) + '*1 = ' + str(one))
+print(str(m) + '*2 = ' + str(two))
+print(str(m) +'*3 = ' + str(three))
+print(str(m) +'*4 = ' + str(four))
+print(str(m) +'*5 = ' + str(five))
+print(str(m) +'*6 = ' + str(six))
+print(str(m) +'*7 = ' + str(seven))
+print(str(m) +'*8 = ' + str(eight))
+print(str(m) +'*9 = ' + str(nine))
+print(str(m) +'*10 = ' + str(ten))
